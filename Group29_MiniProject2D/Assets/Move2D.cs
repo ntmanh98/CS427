0cec81ba3d9149bbde1220c94d221d8fe825e915
Minh Anh <ntmanh@apcs.vn>
2019-07-20 08:17:35 +0700

    Mini Project 2D

---
+++ Group29_MiniProject2D/Assets/Move2D.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2D : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    //public bool isGrounded = false;
+    // Start is called before the first frame update
+    float lockPos = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(lockPos, lockPos, lockPos);
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 7f), ForceMode2D.Impulse);
+        }
+    }
+}
